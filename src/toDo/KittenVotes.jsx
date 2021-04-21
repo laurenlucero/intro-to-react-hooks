071d469f82f6601b9be69192648fc2b72c58d68e
“Lauren <lucerolaurenm@gmail.com>
2021-04-21 19:11:44 -0400

    useState buttons

--- src/toDo/KittenVotes.jsx
+++ src/toDo/KittenVotes.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Card, Button, CardGrid} from '@edx/paragon';
+import React, { useState } from "react";
+import { Card, Button, CardGrid } from "@edx/paragon";
 
 /* INSTRUCTIONS for useState
     Using the `useState` hook, create a buttons that increment each kitten's vote count
@@ -12,41 +12,55 @@ import { Card, Button, CardGrid} from '@edx/paragon';
 */
 
 const KittenVotes = () => {
-
+  const [meowserClicks, setMeowserClicks] = useState(0);
+  const [princessClicks, setPrincessClicks] = useState(0);
 
   return (
     <>
       <h1>useState</h1>
-      <div className="py-3">Meowser was clicked 0 times!</div>
-      <div className="py-3">Princess Purr was clicked 0 times!</div>
       {/* Display vote counts here */}
+      <div className="py-3">Meowser was clicked {meowserClicks} times!</div>
+      <div className="py-3">
+        Princess Purr was clicked {princessClicks} times!
+      </div>
       <CardGrid>
-        <Card style={{ width: '18rem' }}>
-
+        <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src="http://placekitten.com/200/300" />
           <Card.Body>
-          <Card.Title>Meowser</Card.Title>
-            <Card.Text>
-              Vote for Meowser!
-            </Card.Text>
+            <Card.Title>Meowser</Card.Title>
+            <Card.Text>Vote for Meowser!</Card.Text>
             {/* Meowser button here */}
+            <Button
+              className="mr-2"
+              variant="primary"
+              onClick={() =>
+                setMeowserClicks((meowserClicks) => meowserClicks + 1)
+              }
+            >
+              Click Me
+            </Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src="http://placekitten.com/200/299" />
           <Card.Body>
-          <Card.Title>Princess Purr</Card.Title>
-            <Card.Text>
-              Vote for Princess Purr!
-            </Card.Text>
+            <Card.Title>Princess Purr</Card.Title>
+            <Card.Text>Vote for Princess Purr!</Card.Text>
             {/* Princess Purr button here */}
+            <Button
+              className="mr-2"
+              variant="primary"
+              onClick={() =>
+                setPrincessClicks((princessClicks) => princessClicks + 1)
+              }
+            >
+              Click Me
+            </Button>
           </Card.Body>
         </Card>
       </CardGrid>
-
-
     </>
-  )
+  );
 };
 
 export default KittenVotes;
