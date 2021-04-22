44c2c9c5aac6fae26badb627ffdc48c476131165
“Lauren <lucerolaurenm@gmail.com>
2021-04-22 15:44:22 -0400

    useContext practice

--- src/finished/TwoButtons.jsx
+++ src/finished/TwoButtons.jsx
@@ -1,29 +1,39 @@
-import React, {useState, useContext } from 'react';
-import { Button } from '@edx/paragon';
+import React, { useState, useContext } from "react";
+import { Button } from "@edx/paragon";
 
-const ClickContext = React.createContext()
+const ClickContext = React.createContext();
 
 const ClickContextProvider = ({ children }) => {
   const [count, setCount] = useState(0);
-  return <ClickContext.Provider value={[count, setCount]}>{children}</ClickContext.Provider>
-}
+  return (
+    <ClickContext.Provider value={[count, setCount]}>
+      {children}
+    </ClickContext.Provider>
+  );
+};
 
 const ContextButton = ({ text, className }) => {
   const [count, setCount] = useContext(ClickContext);
 
-  return <Button className={className} onClick={() => setCount((prevCount) => prevCount + 1)}>{text}</Button>
-}
+  return (
+    <Button
+      className={className}
+      onClick={() => setCount((prevCount) => prevCount + 1)}
+    >
+      {text}
+    </Button>
+  );
+};
 
 const ClickReport = () => {
-  const [count] = useContext(ClickContext)
-  return <p>The total count is {count}</p>
-}
+  const [count] = useContext(ClickContext);
+  return <p>The total count is {count}</p>;
+};
 
 const ClearButton = () => {
   const [count, setCount] = useContext(ClickContext);
-  return <Button onClick={() => setCount(0)}>Clear</Button>
-}
-
+  return <Button onClick={() => setCount(0)}>Clear</Button>;
+};
 
 const TwoButtons = () => {
   return (
@@ -31,12 +41,12 @@ const TwoButtons = () => {
       <h1>useContext</h1>
       <ClickContextProvider>
         <ClickReport />
-        <ContextButton className="mr-2" text="Click me"/>
-        <ContextButton className="mr-2" text="Click me too!"/>
+        <ContextButton className="mr-2" text="Click me" />
+        <ContextButton className="mr-2" text="Click me too!" />
         <ClearButton />
       </ClickContextProvider>
     </>
-  )
+  );
 };
 
-export default TwoButtons
+export default TwoButtons;
--- src/toDo/TwoButtons.jsx
+++ src/toDo/TwoButtons.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import { Button } from '@edx/paragon'
+import React, { useState, useContext } from "react";
+import { Button } from "@edx/paragon";
 
 /* INSTRUCTIONS for useContext
   Implement two components that consume the ClickContext:
@@ -13,17 +13,31 @@ import { Button } from '@edx/paragon'
 const ClickContext = React.createContext();
 
 const ClickContextProvider = ({ children }) => {
-  const [count, setCount] = useState(0)
-  return <ClickContext.Provider value={[count, setCount]}>{children}</ClickContext.Provider>
-}
+  const [count, setCount] = useState(0);
+  return (
+    <ClickContext.Provider value={[count, setCount]}>
+      {children}
+    </ClickContext.Provider>
+  );
+};
 
-const ContextButton = () => {
+const ContextButton = ({ text, className }) => {
+  const [count, setCount] = useContext(ClickContext);
 
-}
+  return (
+    <Button
+      className={className}
+      onClick={() => setCount((prevCount) => prevCount + 1)}
+    >
+      {text}
+    </Button>
+  );
+};
 
 const ClickReport = () => {
-
-}
+  const [count] = useContext(ClickContext);
+  return <div>There have been {count} clicks!</div>;
+};
 
 const TwoButtons = () => {
   return (
@@ -31,10 +45,13 @@ const TwoButtons = () => {
       <h1>useContext</h1>
       <ClickContextProvider>
         {/* Put Buttons and ClickReport here */}
+        <ClickReport />
+        <ContextButton text="Click me!" />
+        <ContextButton text="Click me 2!" />
       </ClickContextProvider>
       {/* What happens if you put buttons here? */}
     </>
-  )
+  );
 };
 
-export default TwoButtons
+export default TwoButtons;
